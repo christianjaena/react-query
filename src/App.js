e7e7e5562276c1b6c02a46ef04d5fd8853ef4eda
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-09-15 14:10:02 +0800

    added useQuery hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import "./styles.css";
+import {useQuery} from 'react-query';
 
+const fetchPlanets = async () => {
+  const res = await fetch('https://swapi.dev/api/planets/');
+  return res.json();
+}
 export default function App() {
+  const {data, status} = useQuery('planets', fetchPlanets);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      {status === 'error' && (
+        <div>Error</div>
+      )}
+      {status === 'loading' && (
+        <div>Loading</div>
+      )}
+      {status === 'success' && (
+        data.results.map(planet => (
+          <div key={planet.name}>
+            <h2>{planet.name}</h2>
+            <h3>{planet.population}</h3>
+            <h3>{planet.terrain}</h3>
+          </div>
+        ))
+      )}
+      
     </div>
   );
 }
